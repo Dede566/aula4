c8d07e2250eab4dbd2884fec37499c1ec7d359d9
LAB02-PC01 <LAB02-PC01@DESKTOP-A1R01FE>
2024-03-04 21:33:48 -0300

    Atualizando classes
    luiz henrique fez junto

---
+++ Aula_4/src/App/Main.java
@@ -0,0 +1,54 @@
+package App;
+
+import View.Mensagens;
+import model.Banco;
+import model.Usuario;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		Mensagens mensagem = new Mensagens ();
+		
+		String Nome, CPF, Celular, Email, Senha;
+		String opcao;
+		int opcaoNum;
+				
+		do {
+			
+			opcao = 
+			mensagem.solicitarEntrada
+					("Sistema Bancário"
+					+ "\nDigite a opção desejada"
+					+"\n1. Cadastrar Banco"
+					+"\n2. Cadastrar Usuário"
+					+"\n3. Sair"
+					);
+			
+			opcaoNum = Integer.parseInt(opcao);
+			
+			switch(opcaoNum) {
+			case 1:
+				String nomeBanco;
+				nomeBanco = mensagem.solicitarEntrada("Digite o Nome do Banco");
+					Banco novoBanco = new Banco(nomeBanco);
+					mensagem.mostrarMensagemDeSucesso(
+							"Banco Cadastrado com sucesso"
+							+ novoBanco.getBanco()
+							);
+				break;
+			}
+			
+			Nome = mensagem.solicitarEntrada("Digite seu nome");
+			CPF = mensagem.solicitarEntrada("Digite seu CPF");
+			Celular = mensagem.solicitarEntrada("Digite seu Celular");
+			Email = mensagem.solicitarEntrada("Digite seu Email");
+			Senha = mensagem.solicitarEntrada("Digite seu Senha");
+			Usuario usuario = new Usuario(Nome, CPF, Celular,Email, Senha);
+			mensagem.mostrarMensagemDeSucesso(usuario.getUsuario());
+			opcao = "ok";
+			
+		}while(opcao == "ok");
+		
+	}
+
+}
